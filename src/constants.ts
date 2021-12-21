8743504bc8a1387339d92c9fa77470e86a1aade3
Keita <keita.kakemoto@baues.io>
2021-12-22 08:58:18 +0900

    change theme color

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const SITE_NAME = 'BAUES';
 export const FB_APP_ID = '331619160596063';
 export const FB_PAGE_ID = '130407134318451';
 export const TWITTER_ID = '@BAUES_IO';
-export const THEME_COLOR = '#DC2626';
+export const THEME_COLOR = '#111827';
